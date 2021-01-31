347fbfd0f094d957ed9953a9575d566dda22dd50
Kyle Conel <kyjordev@gmail.com>
2021-01-31 11:40:06 -0500

    BG Music added

---
+++ GGJ2021/Assets/CarSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] cars;
+    public Transform[] carSpawns;
+    float timeBetweenSpawns = 5f;
+
+    private float spawnTimer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCar();
+    }
+
+    private void SpawnCar()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
