71a2b17c4365d22b753030109a156a2ce609dee5
Kyle Conel <kyjordev@gmail.com>
2021-01-31 11:15:47 -0500

    Tipping action and negation

--- GGJ2021/Assets/BalanceItem.cs
+++ GGJ2021/Assets/BalanceItem.cs
@@ -6,24 +6,33 @@ using UnityEngine;
 public class BalanceItem : Item
 {
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private float tipSpeed = 10f;
+    [SerializeField] private float tipSpeed = 1f;
     [SerializeField] private float angleWhenItWillFall = 90f;
+    [SerializeField] Color initialIndicatorColor = Color.green;
+    [SerializeField] private Vector2 moveAxes;
+    [SerializeField] private Transform greatGrandparent;
 
     public override void Start()
     {
         base.Start();
+        
     }
     private void FixedUpdate()
     {
         if (isHeld)
         {
-            transform.Rotate(Vector3.right, tipSpeed * Time.fixedDeltaTime);
-            
+            transform.Rotate(greatGrandparent.right, tipSpeed * Time.fixedDeltaTime, Space.World);
+            heldIndicator.GetComponent<Renderer>().material.color = Color.Lerp(initialIndicatorColor, Color.red, Mathf.Abs(transform.eulerAngles.x/angleWhenItWillFall));
             if (Mathf.Abs(transform.eulerAngles.x) > angleWhenItWillFall)
             {
                base.DropItem();
             }
-        } 
+        }
+
+        if (moveAxes.y > .1f)
+        {
+            transform.Rotate(greatGrandparent.right, -(tipSpeed + 0.1f) * Time.fixedDeltaTime,Space.World);
+        }
     }
 
     public override void OnPickup()
@@ -32,6 +41,8 @@ public class BalanceItem : Item
         try
         {
             characterController = GetComponentInParent<CharacterController>();
+            characterController.axesDelegate += (vec2, sprinting) => { moveAxes = vec2; };
+            greatGrandparent = transform.parent.parent.parent;
         }
         catch (Exception e)
         {
